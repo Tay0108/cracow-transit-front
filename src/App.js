ec6a41f220f9e27bec6c1b53fcac107b258315d7
Tay <kjkosecki@gmail.com>
2018-12-18 00:01:26 +0100

    alpha finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Container from './components/Container';
+import MapContainer from './components/MapContainer';
 
 class App extends Component {
 
@@ -12,9 +12,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getStops();
     this.getTrams();
-    //setInterval(() => this.getTrams(), 5000)
+    this.getStops();
   }
 
   getStops() {
@@ -22,10 +21,11 @@ class App extends Component {
       .then(response => response.json())
       .then(stops => this.setState({ stops: stops.stops }));
   }
+
   getTrams() {
     setInterval(() => fetch('http://localhost:8080/vehicleInfo/vehicles')
       .then(response => response.json())
-      .then(trams => this.setState({ trams: trams.vehicles })), 5000);
+      .then(trams => this.setState({ trams: trams.vehicles })), 8000);
   }
 
   render() {
@@ -38,18 +38,19 @@ class App extends Component {
     }
 
     if (this.state.stops === undefined) {
-      return ('loading stops...');
+       return ('loading stops...');
     }
+
     if (this.state.trams === undefined) {
       return ('loading trams...');
     }
 
-    let stops = this.state.stops.map((stop) => normalizeMarker(stop));
     let trams = this.state.trams.map((tram) => normalizeMarker(tram));
+    let stops = this.state.stops.map((stop) => normalizeMarker(stop));
 
     return (
       <div className="App">
-        <Container stops={stops} trams={trams} />
+        <MapContainer stops={stops} trams={trams} />
       </div>
     );
   }
